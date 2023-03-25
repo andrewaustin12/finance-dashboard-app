1d19ccb05941b8ea53fab736dce437244f771f4a
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-03-25 17:21:21 -0400

    Added react router and navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
-
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material"
+import Navbar from "@/scenes/navbar"
 
 function App() {
-
+  const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>}/>
+              <Route path="/predictions" element={<div>prediction page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
