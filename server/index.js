fc1f48e523eda2805c1ae879130577811bd084bf
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-03-28 15:00:58 -0400

    Set up KPI routes

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-// import kpiRoutes from "./routes/kpi.js";
+import kpiRoutes from "./routes/kpi.js";
 // import productRoutes from "./routes/product.js";
 // import transactionRoutes from "./routes/transaction.js";
-// import KPI from "./models/KPI.js";
+import KPI from "./models/KPI.js";
 // import Product from "./models/Product.js";
 // import Transaction from "./models/Transaction.js";
-// import { kpis, products, transactions } from "./data/data.js";
+import { kpis } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-// app.use("/kpi", kpiRoutes);
+app.use("/kpi", kpiRoutes);
 // app.use("/product", productRoutes);
 // app.use("/transaction", transactionRoutes);
 
@@ -40,10 +40,12 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
     // Product.insertMany(products);
     // Transaction.insertMany(transactions);
+    
     console.log()
   })
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(`${error} did not connect...`));
